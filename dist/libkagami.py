8319fb26136f2f47bad75ffa359ed39e79aa3525
albert <030bug@gmail.com>
2017-12-26 17:26:17 +0000

    improve libkagami interface

---
+++ dist/libkagami.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#  -*- coding: utf-8 -*-
+
+"""
+kagami lib interface
+
+author(s): Albert (aki) Zhou
+origin: 12-26-2017
+
+"""
+
+
+import sys
+sys.path.insert(0, 'libkagami.zlib')
+
