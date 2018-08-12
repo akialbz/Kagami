739179e1eb26a65809fb1fd5b96dbf38175e40fe
albert <030bug@gmail.com>
2018-08-12 23:10:36 +0100

    add factor type

--- kagami/core/dtypes/factor.py
+++ kagami/core/dtypes/factor.py
@@ -10,67 +10,116 @@ origin: 08-08-2018
 """
 
 
-import logging
 import numpy as np
-from bidict import OrderedBidict, FrozenOrderedBidict
-from kagami.core.prelim import NA, hasvalue, isna, checkany, hashable
-from kagami.core.functional import partial
+from string import join
+from bidict import FrozenOrderedBidict
+from kagami.core.prelim import NA, hasvalue, optional, checkany, listable, mappable
 
 
-class Factor(object):
-    __slots__ = ('_array', '_levdct', '_valbase')
-    def __init__(self, array, levels = NA, values = NA, mutable = False):
-        if hasvalue(levels) and checkany(array, lambda x: x not in levels): raise ValueError('value(s) not in levels')
-        if hasvalue(levels) and len(levels) != len(set(levels)): raise ValueError('levels have duplications')
-        if hasvalue(levels) and checkany(levels, lambda x: not hashable(x)): raise TypeError('levels not hashable')
-        if hasvalue(values) and len(values) != len(set(values)): raise ValueError('values have duplications')
-        if hashable(values) and checkany(values, lambda x: not isinstance(x, int)): raise TypeError('values are not integers')
-        if hasvalue(levels) and hasvalue(values) and len(levels) != len(values): raise ValueError('levels and values size not match')
-        if hasvalue(values) and isna(levels): logging.warning('levels not given, ignore level value assignment')
+class _Factor(object):
+    __slots__ = ('_array', '_levdct', '_enctype')
 
-        if isna(levels): levels = list(set(array))
-        if isna(values): values = range(len(levels))
-        if len(levels) == 0: raise ValueError('no level assigned')
+    def __init__(self, array = NA, arrValues = NA):
+        if hasvalue(array):
+            self._array = np.array(self._mapLevel(array), dtype = self._enctype)
+        elif hasvalue(arrValues):
+            if checkany(arrValues, lambda x: x not in self._levdct.values()): raise ValueError('array values not recognised')
+            self._array = np.array(arrValues, dtype = self._enctype)
+        else:
+            self._array = np.array([], dtype = self._enctype)
 
-        self._levdct = (OrderedBidict if mutable else FrozenOrderedBidict)(sorted(zip(levels, values), key = lambda x: x[1]))
-        self._array = np.array([self._levdct[v] for v in array])
-        self._valbase = np.min(self._array)
-        
+    # privates
+    def _mapLevel(self, levels):
+        if isinstance(levels, self.__class__):
+            arr = levels.arrValues
+        elif listable(levels):
+            arr = [self._levdct[v] for v in levels]
+        else:
+            arr = self._levdct[levels]
+        return arr
+
+    # build-ins
+    def __getitem__(self, item):
+        return self.__class__(arrValues = self._array[item])
+
+    def __setitem__(self, key, value):
+        self._array.__setitem__(key, self._mapLevel(value))
+
+    def __iter__(self):
+        return iter(self.array)
+
+    def __contains__(self, item):
+        return item in self._levdct.keys()
+
+    def __len__(self):
+        return self.size
+
+    def __str__(self):
+        arr = self.array
+        if len(arr) > 10: arr = list(arr[:8]) + ['...'] + list(arr[-2:])
+        s = 'Factor[' + join(arr, ', ') + ']\n' + \
+            'levels [%d]: %s' % (len(self._levdct), self.levels)
+        return s
+
+    def __repr__(self):
+        return str(self)
+
+    def __array__(self):
+        return self.array
+
+    # properties
+    @property
+    def levels(self):
+        return self._levdct.keys()
+
+    @property
+    def values(self):
+        return self._levdct.values()
+
+    @property
+    def items(self):
+        return self._levdct.items()
+
+    @property
+    def array(self):
+        return np.array([self._levdct.inv[v] for v in self._array])
+
+    @property
+    def arrValues(self):
+        return self._array.copy()
+
+    @property
+    def size(self):
+        return self._array.shape[0]
+
+    # publics
+    @classmethod
+    def encode(cls, array):
+        return np.array([cls._levdct[v] for v in array], dtype = cls._enctype)
+
+    @classmethod
+    def decode(cls, arrValues):
+        return np.array([cls._levdct.inv[v] for v in arrValues])
+
+    def insert(self, other, ids = NA):
+        return self.__class__(arrValues = np.insert(self._array, optional(ids, self.size), self._mapLevel(other)))
+
+    def drop(self, ids):
+        return self.__class__(arrValues = np.delete(self._array, ids))
 
 
-#
-    #
-    #
-    # fct = type(name, (object,), {'__slots__': tuple(levels) + ('_valdct', 'nlevels', 'levels', 'values', 'items', 'encode', 'decode')})
-    #
-    # # privates
-    #
-    #
-    # # build-ins
-    # fct.__len__ = lambda self: len(self._valdct)
-    #
-    # # properties
-    # def _getter(self, lev):
-    #     return self
-    # def _setter(self, val, lev):
-    #     self._valdct[lev] = val
-    # for l in levels: setattr(fct, l, property(partial(_getter, lev = l), partial(_setter, lev = l)))
-    #
-    # fct.nlevels = property(lambda self: len(self._valdct))
-    # fct.levels = property(lambda self: self._valdct.keys())
-    # fct.values = property(lambda self: self._valdct.values())
-    # fct.items = property(lambda self: self._valdct.items())
-    #
-    # # methods
-    # fct.encode = classmethod(lambda cls, val: [cls._valdct[v] for v in val])
-    # fct.decode = classmethod(lambda cls, val: [cls._valdct.inv[v] for v in val])
-    #
-    # return fct
-    #
+def factor(name, levels, enctype = np.uint32):
+    fct = type(name, (_Factor,), {})
 
-if __name__ == '__main__':
-    Color = factor('Colors', ('r', 'g', 'b'), mutable =  True)
-    xx = [Color.r, Color.g, Color.b]
-    import pdb; pdb.set_trace()
+    if listable(levels):
+        if len(levels) != len(set(levels)): raise ValueError('levels have duplications')
+        fct._levdct = FrozenOrderedBidict([(v, i) for i, v in enumerate(levels)])
+    elif mappable(levels):
+        if checkany(levels.values(), lambda x: not isinstance(x, int)): raise TypeError('level values are not integers')
+        fct._levdct = FrozenOrderedBidict([(v, i) for v, i in levels.items()])
+    else:
+        raise TypeError('unknown levels type: %s' % str(type(levels)))
 
+    fct._enctype = enctype
+    return fct
 
