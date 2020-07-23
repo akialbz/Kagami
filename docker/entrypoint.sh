1795bf14f29c6e2e90362ed5832c81e71b950f4f
akialbert <030bug@gmail.com>
2020-07-23 15:10:27 +0100

    clean README.md for version 3 release

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-if [[ $1 == 'test' ]]; then
+if [[ $1 == 'python' ]]; then
+    python
+elif [[ $1 == 'test' ]]; then
     python -c "import kagami; kagami.test()"
 elif [[ "$#" -eq 0 ]] || [[ ! -f $1 ]]; then
     echo "ERROR: no input file or invalid input file name [[ $1 ]]"
